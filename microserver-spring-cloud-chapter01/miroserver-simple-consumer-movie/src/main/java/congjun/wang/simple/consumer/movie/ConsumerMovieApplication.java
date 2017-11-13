dd63e7b1002f9103b38ccd7d00778ee64dd10175
wcjwctwy <708808508@qq.com>
2017-11-13 23:01:18 +0800

    简单实例-电影服务消费者

---
+++ microserver-spring-cloud-chapter01/miroserver-simple-consumer-movie/src/main/java/congjun/wang/simple/consumer/movie/ConsumerMovieApplication.java
@@ -0,0 +1,11 @@
+package congjun.wang.simple.consumer.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMovieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class,args);
+    }
+}
