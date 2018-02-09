0dd522086cc1cd8057a07489ad3959633ec19739
wcjwctwy <708808508@qq.com>
2018-02-09 17:32:11 +0800

    创建完成spring cloud stream 结构

---
+++ microserver-spring-cloud-chapter07/spring-cloud-07-stream-comsumer/src/main/java/wang/congjun/StreamProduceApplication.java
@@ -0,0 +1,9 @@
+package wang.congjun;
+
+/**
+ * @author WangCongJun
+ * @date 2018/2/9
+ * Created by WangCongJun on 2018/2/9.
+ */
+public class StreamProduceApplication {
+}
---
+++ microserver-spring-cloud-chapter07/spring-cloud-07-stream-produce/src/main/java/wang/congjun/SreamComsumerApplication.java
@@ -0,0 +1,12 @@
+package wang.congjun;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WangCongJun
+ * @date 2018/2/9
+ * Created by WangCongJun on 2018/2/9.
+ */
+@SpringBootApplication
+public class SreamComsumerApplication {
+}
